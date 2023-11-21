d907ca17b7725e7cae189fd8bf1ca8d37ab84a8e
pfrances <pfrances@student.42tokyo.jp>
2023-11-21 15:47:57 +0900

    add user/all endpoint

---
+++ HttpEndpoints/user/allUsers.ts
@@ -0,0 +1,26 @@
+import {HttpUserEndPointBase} from '.';
+import {HttpMethod} from '../enum';
+import {UserPublicProfile} from '../interfaces';
+import {ARequestSender} from '../interfaces/ARequestSender';
+
+export namespace HttpAllUsers {
+  export const method = HttpMethod.GET;
+  export const endPoint = '/all';
+  export const endPointFull = `${HttpUserEndPointBase}${endPoint}`;
+
+  export class reqTemplate {}
+
+  export class resTemplate {
+    public users: UserPublicProfile[];
+
+    constructor(users: UserPublicProfile[]) {
+      this.users = users;
+    }
+  }
+
+  export class requestSender extends ARequestSender<reqTemplate, resTemplate> {
+    constructor(authToken: string) {
+      super(endPointFull, method, reqTemplate, resTemplate, authToken);
+    }
+  }
+}
--- HttpEndpoints/user/index.ts
+++ HttpEndpoints/user/index.ts
@@ -2,18 +2,20 @@ export const HttpUserEndPointBase = '/user';
 
 import {HttpGetMe} from './me';
 import {HttpEditMe} from './edit';
+import {HttpAllUsers} from './allUsers';
 
-export {HttpGetMe, HttpEditMe};
+export {HttpGetMe, HttpEditMe, HttpAllUsers};
 
 export namespace HttpUser {
   export import getMe = HttpGetMe;
   export import editMe = HttpEditMe;
+  export import allUsers = HttpAllUsers;
 
   export const endPointBase = HttpUserEndPointBase;
 
-  export type reqTemplate = getMe.reqTemplate | editMe.reqTemplate;
+  export type reqTemplate = getMe.reqTemplate | editMe.reqTemplate | allUsers.reqTemplate;
 
-  export type resTemplate = getMe.resTemplate | editMe.resTemplate;
+  export type resTemplate = getMe.resTemplate | editMe.resTemplate | allUsers.resTemplate;
 
-  export type requestSender = getMe.requestSender | editMe.requestSender;
+  export type requestSender = getMe.requestSender | editMe.requestSender | allUsers.requestSender;
 }
